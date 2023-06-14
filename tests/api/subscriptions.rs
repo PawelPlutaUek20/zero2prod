78ea8dce1958341a86e46f9b879d89e64240a3e7
PawelPlutaUek20 <pawelpluta58@gmail.com>
2023-06-15 00:05:58 +0200

    Chapter 8 - Error handling

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -44,7 +44,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     let app = spawn_app().await;
 
     let test_cases = vec![
@@ -86,11 +86,6 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 async fn subscribe_persists_the_new_subscriber() {
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    Mock::given(path("/emails"))
-        .and(method("POST"))
-        .respond_with(ResponseTemplate::new(200))
-        .mount(&app.email_server)
-        .await;
 
     app.post_subscriptions(body.into()).await;
 
@@ -121,4 +116,18 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
     let confirmation_links = app.get_confirmation_links(&email_request);
 
     assert_eq!(confirmation_links.html, confirmation_links.plain_text)
+}
+
+#[tokio::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    sqlx::query!("ALTER TABLE subscriptions DROP COLUMN email;",)
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    let response = app.post_subscriptions(body.into()).await;
+
+    assert_eq!(response.status().as_u16(), 500);
 }
